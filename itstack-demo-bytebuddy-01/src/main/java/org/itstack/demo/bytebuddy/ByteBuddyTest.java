479966315864aee76b8b0abf29d792b9fdea89bf
袁江朋 <yuanjiangpeng@zuzuche.com>
2020-06-22 15:00:21 +0800

    clean

---
+++ itstack-demo-bytebuddy-01/src/main/java/org/itstack/demo/bytebuddy/ByteBuddyTest.java
@@ -0,0 +1,13 @@
+package org.itstack.demo.bytebuddy;
+
+/**
+ * @author Jenpin
+ * @date 2020/6/22 14:59
+ * @description
+ **/
+public class ByteBuddyTest {
+
+    public static void main(String[] args) {
+        System.out.println("ByteBuddyTest");
+    }
+}
